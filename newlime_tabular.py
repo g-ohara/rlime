5112636c0c40e7e0c5a9ec73d3706a5d95a6b6d2
Genji Ohara <oharagenji1024@gmail.com>
2023-11-21 23:50:38 +0900

    Splitted newlime_tabular.NewLimeTabularExplainer.get_sample_fn into some submodules

--- newlime_tabular.py
+++ newlime_tabular.py
@@ -1,17 +1,27 @@
 """NewLIME for tabular datasets"""
 
-import typing
+import dataclasses
 
 import numpy as np
 import pandas as pd
 from anchor import anchor_explanation, anchor_tabular
 from river import compose
 
-from . import newlime_base
-from .newlime_base import Anchor, Classifier, NewLimeBaseBeam, Sample, SampleFn
+import newlime_base
+from newlime_base import (Anchor, Classifier, HyperParam, NewLimeBaseBeam,
+                          Sample, SampleFn)
 
 Predicate = tuple[int, str, int]
 Mapping = dict[int, Predicate]
+
+
+@dataclasses.dataclass
+class Conditions:
+    """Class for conditions of equality and inequality"""
+
+    eq: dict[int, int]
+    leq: dict[int, int]
+    geq: dict[int, int]
 
 
 class NewLimeTabularExplainer(anchor_tabular.AnchorTabularExplainer):
@@ -26,6 +36,57 @@ class NewLimeTabularExplainer(anchor_tabular.AnchorTabularExplainer):
             discretized.
     """
 
+    @staticmethod
+    def get_conditions(
+        present: newlime_base.Rule, mapping: Mapping
+    ) -> Conditions:
+        """Classify conditions in given rule into eqality, inequality(<) and
+        inequality(>)"""
+
+        conditions_eq: dict[int, int] = {}
+        conditions_leq: dict[int, int] = {}
+        conditions_geq: dict[int, int] = {}
+
+        x: int  # a predicate index in given rule
+        for x in present:
+            f: int  # feature index (f == x ??)
+            op: str  # comparison operator
+            v: int  # standard value
+            f, op, v = mapping[x]
+            if op == "eq":
+                conditions_eq[f] = v
+            elif op == "leq":
+                if f not in conditions_leq:
+                    conditions_leq[f] = v
+                conditions_leq[f] = min(conditions_leq[f], v)
+            elif op == "geq":
+                if f not in conditions_geq:
+                    conditions_geq[f] = v
+                conditions_geq[f] = max(conditions_geq[f], v)
+        # conditions_eq = dict([(x, data_row[x]) for x in present])
+        return Conditions(conditions_eq, conditions_leq, conditions_geq)
+
+    @staticmethod
+    def restore_data(
+        data_row: np.ndarray,
+        d_raw_data: np.ndarray,
+        num_samples: int,
+        mapping: Mapping,
+    ) -> np.ndarray:
+        """Restore original representations for new data points"""
+
+        data: np.ndarray = np.zeros((num_samples, len(mapping)), int)
+        for i, predicate in mapping.items():
+            f, op, v = predicate
+            if op == "eq":
+                data[:, i] = (d_raw_data[:, f] == data_row[f]).astype(int)
+            if op == "leq":
+                data[:, i] = (d_raw_data[:, f] <= v).astype(int)
+            if op == "geq":
+                data[:, i] = (d_raw_data[:, f] > v).astype(int)
+        # data = (raw_data == data_row).astype(int)
+        return data
+
     def get_sample_fn(
         self,
         data_row: np.ndarray,  # target instance
@@ -33,20 +94,45 @@ class NewLimeTabularExplainer(anchor_tabular.AnchorTabularExplainer):
         desired_label: int | None = None,
     ) -> tuple[SampleFn, Mapping]:
         """Return sampling function that gets sample from neighborhood of
-        target instance. The function returns ......"""
+        target instance. The function returns ......
 
-        # Get predictions of the blackbox classifier
+        Parameters
+        ----------
+        data_row : np.ndarray
+            Target instance.
+        classifier_fn: Classifier
+            Blackbox classifier that labels new data points.
+        desired_label: int | None, default None
+            Label that new data points are required to have.
+
+        Returns
+        -------
+        sample_fn : SampleFn
+        mapping : dict[int, tuple[int, str, int]]
+            Set of conditions in target instance (data_row).
+            f : int
+                index of feature
+            op : str
+                comparison operator
+            v : int
+                index of categorical name
+        """
+
         def predict_fn(x: np.ndarray) -> np.ndarray:
+            """Get predictions of the blackbox classifier"""
             return classifier_fn(self.encoder_fn(x))
 
         # must map present here to include categorical features
         # (for conditions_eq), and numerical features for geq and leq
         mapping: Mapping = {}
 
+        # Discritize the target instance.
         data_row = self.disc.discretize(data_row.reshape(1, -1))[0]
 
         for f in self.categorical_features:
             if f in self.ordinal_features:
+                # Add f to mapping as inequality condition if f is ordinal
+                # categorical feature.
                 for v in range(len(self.categorical_names[f])):
                     idx = len(mapping)
                     if (
@@ -54,16 +140,13 @@ class NewLimeTabularExplainer(anchor_tabular.AnchorTabularExplainer):
                         and v != len(self.categorical_names[f]) - 1
                     ):
                         mapping[idx] = (f, "leq", v)
-                        # names[idx] = '%s <= %s' % (self.feature_names[f], v)
                     elif data_row[f] > v:
                         mapping[idx] = (f, "geq", v)
-                        # names[idx] = '%s > %s' % (self.feature_names[f], v)
             else:
+                # Add f to mapping as equality condition if f is non-ordinal
+                # categorical feature.
                 idx = len(mapping)
                 mapping[idx] = (f, "eq", int(data_row[f]))
-            # names[idx] = '%s = %s' % (
-            #     self.feature_names[f],
-            #     self.categorical_names[f][int(data_row[f])])
         ##
 
         # *********************************************************************
@@ -81,65 +164,63 @@ class NewLimeTabularExplainer(anchor_tabular.AnchorTabularExplainer):
         ) -> Sample:
             """Get sample satisfying given rule and its label predicted by
             the surrogate model, then update the surrogate model for the sample
+
+            Parameters
+            ----------
+            present : tuple[int, ...]
+                Target rule.
+            num_samples : int
+                The number of returned sample.
+            compute_labels : bool default True
+                Whether compute reward or not
+            surrogate_model: compose.Pipeline | None default None
+                The surrogate model that will be updated on sampling.
+                If None, it does not compute reward or update the model
+                regardless of the value of surrogate_model and update_model.
+            update_model : bool default True
+                Whether update the surrogate model on sampling or not
+
+            Returns
+            -------
+            Sample
+                Data points sampled from neighborhood of the target rule
+                (present).
+                raw_data : np.ndarray
+                    Original data points sampled from neighborhood of the
+                    target rule.
+                d_raw_data : np.ndarray
+                    Discretized data points (Discretized raw_data).
+                data : np.ndarray
             """
 
-            conditions_eq: dict[int, int] = {}
-            conditions_leq: dict[int, int] = {}
-            conditions_geq: dict[int, int] = {}
-
-            x: int  # a predicate index in given rule
-            for x in present:
-                f: int  # feature index (f == x ??)
-                op: str  # comparison operator
-                v: int  # standard value
-                f, op, v = mapping[x]
-                if op == "eq":
-                    conditions_eq[f] = v
-                if op == "leq":
-                    if f not in conditions_leq:
-                        conditions_leq[f] = v
-                    conditions_leq[f] = min(conditions_leq[f], v)
-                if op == "geq":
-                    if f not in conditions_geq:
-                        conditions_geq[f] = v
-                    conditions_geq[f] = max(conditions_geq[f], v)
-            # conditions_eq = dict([(x, data_row[x]) for x in present])
+            conditions = NewLimeTabularExplainer.get_conditions(
+                present, mapping
+            )
 
             # Sample new data points
             raw_data: np.ndarray = self.sample_from_train(
-                conditions_eq, {}, conditions_geq, conditions_leq, num_samples
+                conditions.eq, {}, conditions.geq, conditions.leq, num_samples
             )
 
             # Descretize new data points
             d_raw_data: np.ndarray = self.disc.discretize(raw_data)
 
             # Restore original representations for new data points
-            data: np.ndarray = np.zeros((num_samples, len(mapping)), int)
-
-            for i, predicate in mapping.items():
-                f, op, v = predicate
-                if op == "eq":
-                    data[:, i] = (d_raw_data[:, f] == data_row[f]).astype(int)
-                if op == "leq":
-                    data[:, i] = (d_raw_data[:, f] <= v).astype(int)
-                if op == "geq":
-                    data[:, i] = (d_raw_data[:, f] > v).astype(int)
-            # data = (raw_data == data_row).astype(int)
+            data = NewLimeTabularExplainer.restore_data(
+                data_row, d_raw_data, num_samples, mapping
+            )
 
             # *****************************************************************
             labels: np.ndarray = np.array([])
             if surrogate_model is not None:
-                given_model: compose.Pipeline = typing.cast(
-                    compose.Pipeline, surrogate_model
-                )
                 data_x: pd.DataFrame = pd.DataFrame(raw_data)
                 data_y = pd.Series(predict_fn(raw_data))
                 if compute_labels:
                     labels = (
-                        given_model.predict_many(data_x) == data_y
+                        surrogate_model.predict_many(data_x) == data_y
                     ).astype(int)
                 if update_model:
-                    given_model.learn_many(data_x, data_y)
+                    surrogate_model.learn_many(data_x, data_y)
             # *****************************************************************
 
             return Sample(raw_data, data, labels)
@@ -148,34 +229,23 @@ class NewLimeTabularExplainer(anchor_tabular.AnchorTabularExplainer):
 
         return sample_fn, mapping
 
-    def explain_instance(
+    def my_explain_instance(
         self,
         data_row: np.ndarray,
         classifier_fn: Classifier,
-        threshold: float = 0.95,
-        delta: float = 0.1,
-        epsilon: float = 0.15,
-        batch_size: int = 100,
-        beam_size: int = 4,
-        max_rule_length: int | None = None,
+        hyper_param: HyperParam,
     ) -> (
         tuple[anchor_explanation.AnchorExplanation, compose.Pipeline | None]
         | None
     ):
+        """Generate NewLIME explanation for given classifier on neighborhood of
+        given data point"""
+
         # It's possible to pass in max_anchor_size
         sample_fn, mapping = self.get_sample_fn(data_row, classifier_fn)
 
         # *********************************************************************
         # Generate Explanation
-        hyper_param = newlime_base.HyperParam(
-            delta=delta,
-            epsilon=epsilon,
-            batch_size=batch_size,
-            desired_confidence=threshold,
-            beam_size=beam_size,
-            max_rule_length=max_rule_length,
-        )
-
         result: tuple[Anchor, compose.Pipeline | None] | None
         result = NewLimeBaseBeam.beam_search(sample_fn, hyper_param)
         # *********************************************************************
@@ -193,5 +263,3 @@ class NewLimeTabularExplainer(anchor_tabular.AnchorTabularExplainer):
             "tabular", exp, self.as_html
         )
         return explanation, surrogate_model
-
-    ##
